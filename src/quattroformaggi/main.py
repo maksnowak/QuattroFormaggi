350530e9e5930e3eb27a941672d4f66a33e971c9
Pitygiusz <piotrek@jurczyk.biz>
2026-04-18 23:23:25 +0000

    main pipeline update

--- src/quattroformaggi/main.py
+++ src/quattroformaggi/main.py
@@ -4,14 +4,29 @@ def run():
     from dotenv import load_dotenv
 
     from quattroformaggi.QueryInterpreter import interpret_query
+    from query_to_sql import filter_humanitarian_data
+
+    import pandas as pd
 
     load_dotenv()
 
-    result = asyncio.run(
-        interpret_query("show underfunded food crises in the Sahel since 2022")
+    user_query = "show underfunded food crises in the Sahel since 2022"
+
+    agent1_result = asyncio.run(
+        interpret_query(user_query)
     )
 
+    agent_json = agent1_result.model_dump_json()
 
+    master_table_path = "unocha.default.master_table"
 
-    
-    print(result.model_dump_json())
+    filtered_data = filter_humanitarian_data(agent_json, spark, master_table_path)
+
+    data_as_csv_string = filtered_df.toPandas().to_csv(index=False)
+
+    agent2_result = asyncio.run(
+        brief_writer(data_as_csv_string, user_query)
+    )
+
+    print(agent2_result)
+
